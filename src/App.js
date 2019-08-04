e2cb97e817e2cf5f932dd28affce0d7f4d4b0273
Arthur <arr90rj@gmail.com>
2019-08-04 16:23:34 -0300

    add & config redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
 function App() {
     return (
-        <>
+        <Provider store={store}>
             <GlobalStyle />
             <Routes />
-        </>
+        </Provider>
     );
 }
 
